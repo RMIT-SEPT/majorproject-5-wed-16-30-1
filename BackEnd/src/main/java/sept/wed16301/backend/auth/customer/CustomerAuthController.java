e2bcb2603b0847274a5e65822a321eeff0a6efcb
[Surinder Kaur s3785282] <s3785282@student.rmit.edu.au>
2020-09-13 17:32:48 +1000

    updated worker and customer controllers

--- BackEnd/src/main/java/sept/wed16301/backend/auth/customer/CustomerAuthController.java
+++ BackEnd/src/main/java/sept/wed16301/backend/auth/customer/CustomerAuthController.java
@@ -5,37 +5,111 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sept.wed16301.backend.User;
 import sept.wed16301.backend.auth.AuthResponse;
 import sept.wed16301.backend.auth.LoginRequest;
 import sept.wed16301.backend.auth.RegisterRequest;
+import sept.wed16301.backend.database.UserDatabase;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 
 @RestController
 public class CustomerAuthController {
 
     @PostMapping("/api/auth/customer/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        AuthResponse response;
+
+        //Connect to database
+        UserDatabase users;
+        try {
+            users = new UserDatabase();
+        } catch (Exception e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
         // Retrieve user from database.
+        User customer = null;
+        try {
+            customer = users.getCustomer(loginRequest.getUsername());
+        } catch(SQLException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
         // If user doesn't exist, return 401 Unauthorized.
+        if(customer == null) {
+            response = new AuthResponse("Invalid username or password.", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Check that password hash matches.
         // If password doesn't match, return 401 Unauthorized.
+        try {
+            boolean passwordMatch = customer.getPasswordHash().equals(loginRequest.getPasswordHash());
+            if(customer == null || !passwordMatch) {
+                response = new AuthResponse("Invalid username or password.", HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<>(response, response.getStatus());
+            }
+        } catch (NoSuchAlgorithmException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Initialise user session.
 
         // Return 200 OK.
-        AuthResponse response = new AuthResponse("Successfully logged in.", HttpStatus.OK);
+        response = new AuthResponse("Successfully logged in.", HttpStatus.OK);
         return new ResponseEntity<>(response, response.getStatus());
     }
 
     @PostMapping("/api/auth/customer/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        AuthResponse response;
+
         // Check if passwords match.
         // If passwords don't match, return 400 Bad Request.
+        if(!registerRequest.passwordMatches()) {
+            response = new AuthResponse("Passwords don't match.", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
+        //Connect to database
+        UserDatabase users;
+        try {
+            users = new UserDatabase();
+        } catch (Exception e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Check if user already exists in database.
+        User customer = null;
+        try {
+            customer = users.getCustomer(registerRequest.getUsername());
+        } catch(SQLException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
         // If user already exists, return 409 Conflict.
+        if(customer != null) {
+            response = new AuthResponse("That username is already taken.", HttpStatus.CONFLICT);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Add user to database if it doesn't exist.
+        try {
+            if(!users.createCustomer(registerRequest)) {
+                response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<>(response, response.getStatus());
+            }
+        } catch (SQLException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Return 201 Created.
         AuthResponse resp = new AuthResponse("Customer successfully created.", HttpStatus.CREATED);
--- BackEnd/src/main/java/sept/wed16301/backend/auth/worker/WorkerAuthController.java
+++ BackEnd/src/main/java/sept/wed16301/backend/auth/worker/WorkerAuthController.java
@@ -1,41 +1,115 @@
 package sept.wed16301.backend.auth.worker;
 
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sept.wed16301.backend.User;
 import sept.wed16301.backend.auth.AuthResponse;
 import sept.wed16301.backend.auth.LoginRequest;
 import sept.wed16301.backend.auth.RegisterRequest;
+import sept.wed16301.backend.database.UserDatabase;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 
 @RestController
 public class WorkerAuthController {
 
     @PostMapping("/api/auth/worker/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
-        // Retrieve user from database.
-        // If user doesn't exist, return 401 Unauthorized.
+        //Capture response
+        AuthResponse response;
 
-        // Check that password hash matches.
-        // If password doesn't match, return 401 Unauthorized.
+        //Open connection to user Database
+        UserDatabase users;
+        try {
+            users = new UserDatabase();
+        } catch(Exception e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
+        //Get the worker from User Database
+        User worker = null;
+        try {
+            worker = users.getWorker(loginRequest.getUsername());
+        } catch (SQLException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
+        //If worker does not exist - return 401 Unauthorized.
+        if(worker == null) {
+            response = new AuthResponse("Invalid username or password.", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
+        //Check if password hash matches, otherwise return 401 Unauthorized
+        try {
+            if(worker == null || !worker.getPasswordHash().equals(loginRequest.getPasswordHash())) {
+                response = new AuthResponse("Invalid username or password.", HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<>(response, response.getStatus());
+            }
+        } catch (NoSuchAlgorithmException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Initialise user session.
 
         // Return 200 OK.
-        AuthResponse response = new AuthResponse("Successfully logged in.", HttpStatus.OK);
+        response = new AuthResponse("Successfully logged in.", HttpStatus.OK);
         return new ResponseEntity<>(response, response.getStatus());
     }
 
     @PostMapping("/api/auth/worker/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        AuthResponse response;
+
         // Check if passwords match.
         // If passwords don't match, return 400 Bad Request.
+        if(!registerRequest.passwordMatches()) {
+            response = new AuthResponse("Passwords don't match.", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
+        //Connect to database
+        UserDatabase users;
+        try {
+            users = new UserDatabase();
+        } catch (Exception e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Check if user already exists in database.
+        User worker = null;
+        try {
+            worker = users.getWorker(registerRequest.getUsername());
+        } catch(SQLException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
+
         // If user already exists, return 409 Conflict.
+        if(worker != null) {
+            response = new AuthResponse("That username is already taken.", HttpStatus.CONFLICT);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Add user to database if it doesn't exist.
+        try {
+            if(!users.createWorker(registerRequest)) {
+                response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<>(response, response.getStatus());
+            }
+        } catch (SQLException e) {
+            response = new AuthResponse("Something went wrong.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(response, response.getStatus());
+        }
 
         // Return 201 Created.
         AuthResponse resp = new AuthResponse("Worker successfully created.", HttpStatus.CREATED);
